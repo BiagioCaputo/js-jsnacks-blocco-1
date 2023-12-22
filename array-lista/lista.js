e3d35dd720667c451fcf2837d18cb9cb480073e1
BiagioCaputo <scebiagio98@gmail.com>
2023-12-22 13:46:00 +0100

    Terminato l'es 1 Tutti i frutti

--- array-lista/lista.js
+++ array-lista/lista.js
@@ -1,2 +1,26 @@
 console.log("JS OK")
 
+//creo il mio array
+const fruits = ["banana", "mela", "pera", "ciliegia", "arancia", "mandarino", "cocomero" , "limone", "fragola"];
+
+console.log(fruits);
+
+//aggiungo la pesca al mio array
+
+fruits.push("pesca");
+
+console.log(fruits);
+
+//controllo se ho il cocomero nell'array dando per scontato di no
+
+let message = "Oh no, devo uscire a comprare il cocomero!";
+
+for (i=0; i<fruits.length; i++){
+
+    if(fruits[i] === "cocomero"){
+        message = "Trovato! Devo solo preparare il cocktail.";
+    }
+
+}
+
+console.log(message);
